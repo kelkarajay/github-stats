7e1de08dc51957be1cd1e5ce1bff5b7727c777b8
Ajay Kelkar <ajaykelkar@outlook.com>
2020-04-06 17:26:50 +0200

    feature: Adding Docker image build capability

--- src/model/github.user.model.ts
+++ src/model/github.user.model.ts
@@ -1,57 +1,57 @@
 export class GithubUserModel{
-    login: String
-	id: number
-	node_id: String
-	avatar_url: String
-	gravatar_id?: String
-	url: String
-	html_url: String
-	followers_url: String
-	following_url: String
-	gists_url: String
-	starred_url: String
-	subscriptions_url: String
-	organizations_url: String
-	repos_url: String
-	events_url: String
-	received_events_url: String
-	type: String
-	site_admin: boolean
-    score: number
+    login: String = ""
+	id: number = 0
+	node_id: String = ""
+	avatar_url: String = ""
+	gravatar_id?: String = ""
+	url: String = ""
+	html_url: String = ""
+	followers_url: String = ""
+	following_url: String = ""
+	gists_url: String = ""
+	starred_url: String = ""
+	subscriptions_url: String = ""
+	organizations_url: String = ""
+	repos_url: String = ""
+	events_url: String = ""
+	received_events_url: String = ""
+	type: String = ""
+	site_admin: boolean = false
+    score: number = 0
 }
 
 export class GithubUserDetailModel{ 
-	login: String
-	id: number
-	node_id: String
-	avatar_url: String
-	gravatar_id: String
-	url: String
-	html_url: String
-	followers_url: String
-	following_url: String
-	gists_url: String
-	starred_url: String
-	subscriptions_url: String
-	organizations_url: String
-	repos_url: String
-	events_url: String
-	received_events_url: String
-	type: String
-	site_admin: boolean
-	name: String
-	company?: String
-	blog: String
-	location: String
-	email: String
+	login: String  = ""
+	id: number = 0
+	node_id: String = ""
+	avatar_url: String = ""
+	gravatar_id: String = ""
+	url: String = ""
+	html_url: String = ""
+	followers_url: String = ""
+	following_url: String = ""
+	gists_url: String = ""
+	starred_url: String = ""
+	subscriptions_url: String = ""
+	organizations_url: String = ""
+	repos_url: String = ""
+	events_url: String = ""
+	received_events_url: String = ""
+	type: String = ""
+	site_admin: boolean = false
+	name: String = ""
+	company?: String = ""
+	blog: String = ""
+	location: String = ""
+	email: String = ""
 	hireable?: String
-	bio: String
-	public_repos: number
-	public_gists: number
-	followers: number
-	following: number
-	created_at: String
-	updated_at: String
+	bio: String = ""
+	public_repos: number = 0
+	public_gists: number = 0
+	followers: number = 0
+	following: number = 0
+	created_at: String = ""
+	updated_at: String = ""
 }
 
 export class GithubUserCollectionModel{
