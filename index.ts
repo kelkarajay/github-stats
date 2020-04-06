402c028310f6d0ee35c47f9cc044e9384f39c870
Ajay Kelkar <ajaykelkar@outlook.com>
2020-04-06 16:45:37 +0200

    feature: Terminate start up if required env variable is not set

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
 import { Server } from "./src/server";
 
-console.log("Hello world!");
+if(!process.env.GH_TOKEN) {
+    console.error("A valid Github Token must be set as an environment variable ( GH_TOKEN ).")
+    process.exit(1);
+}
+
+console.log("Starting Server")
 const x = new Server();
 x.startServer();
